04f5f8ed73195b10982735a8f1a6f2816f75326b
Paulo Vitor Bettini de Paiva Lima <p.bettini@youwe.nl>
2019-03-08 16:21:11 +0100

    first test failing

---
+++ tests/StringCalculatorTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paulo.bettini
+ * Date: 2019-03-08
+ * Time: 16:15
+ */
+
+namespace App\Test;
+
+use StringCalculator;
+
+class StringCalculatorTest extends \PHPUnit_Framework_TestCase
+{
+
+}
